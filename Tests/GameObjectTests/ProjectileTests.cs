da6ad209cec9f049b40d2338f712dcd682aa086d
hubajoo <joohuba@gmail.com>
2024-11-25 06:04:55 +0100

    Working on the tests

--- Tests/GameObjectTests/ProjectileTests.cs
+++ Tests/GameObjectTests/ProjectileTests.cs
@@ -1,86 +1,80 @@
-using DungeonCrawl.GameObjects.ObjectInterfaces;
 using DungeonCrawl.GameObjects;
-using DungeonCrawl.UI;
+using DungeonCrawl.GameObjects.ObjectInterfaces;
 using DungeonCrawl.Mechanics;
-using SadConsole;
-using SadRogue.Primitives;
-using DungeonCrawl.Mechanics;
-using NUnit.Framework;
+using DungeonCrawl.Maps;
 using Moq;
+using NUnit.Framework;
+using SadRogue.Primitives;
+using SadConsole;
 
-namespace Tests.GameObjectTests;
-
-public class Projectile
+namespace Tests.GameObjectTests
 {
-  private GameObject _gameObject;
-  private ColoredGlyph _appearance;
-  private Point _position;
-  private Direction _direction;
-  private Mock<IScreenObjectManager> _screenObjectManager;
-  private Mock<IMap> _map;
-  private Mock<IMovementLogic> _movementLogic;
-
-
-  [SetUp]
-  public void Setup()
+  [TestFixture]
+  public class ProjectileTests
   {
-    _appearance = new ColoredGlyph(Color.White, Color.Black, 'X');
-    _position = new Point(1, 1);
-    _direction = Direction.Down;
+    private Mock<IScreenObjectManager> _mockScreenObjectManager;
+    private Mock<IMap> _mockMap;
+    private Mock<IMovementLogic> _mockMovementLogic;
+    private Projectile _projectile;
+    private Point _position = new Point(1, 1);
+    private int _damage = 5;
+    private int _maxDistance = 10;
+    private Direction _direction = Direction.Right;
+    private Color _color = Color.Red;
 
-    _screenObjectManager = new Mock<IScreenObjectManager>();
-    _map = new Mock<IMap>();
+    [SetUp]
+    public void SetUp()
+    {
+      _mockScreenObjectManager = new Mock<IScreenObjectManager>();
+      _mockMap = new Mock<IMap>();
+      _mockMovementLogic = new Mock<IMovementLogic>();
+      _projectile = new Projectile(_position, _direction, _mockScreenObjectManager.Object, _maxDistance, _damage, _color, _mockMap.Object);
 
-    _movementLogic = new Mock<IMovementLogic>();
-    _movementLogic.Setup(m => m.Move(It.IsAny<IGameObject>(), It.IsAny<IMap>(), It.IsAny<Point>()));
-    _gameObject = new TestGameObject(_appearance, _position, _screenObjectManager.Object, _map.Object, _movementLogic.Object);
+      _projectile.SetMovementLogic(_mockMovementLogic.Object);
+    }
+
+    [Test]
+    public void Constructor_SetsProperties()
+    {
+      Assert.AreEqual(_position, _projectile.Position);
+      Assert.AreEqual(_direction, _projectile.Direction);
+      Assert.AreEqual(_damage, _projectile.Damage);
+      Assert.AreEqual(_color, _projectile.Appearance.Foreground);
+    }
+
+    [Test]
+    public void GetDamage_ReturnsCorrectDamage()
+    {
+      Assert.AreEqual(5, _projectile.GetDamage());
+    }
+
+    [Test]
+    public void Touched_ChangesAppearanceAndStopsProjectile()
+    {
+      var initialAppearance = _projectile.Appearance;
+      _projectile.Touched(new Mock<IGameObject>().Object);
+      Assert.AreNotEqual(initialAppearance, _projectile.Appearance);
+      Assert.AreEqual(Direction.None, _projectile.Direction);
+    }
+
+    [Test]
+    public void Fly_RemovesProjectileWhenMaxDistanceReached()
+    {
+      // _mockMovementLogic.Setup(m => m.Move(It.IsAny<IGameObject>(), It.IsAny<IMap>(), It.IsAny<Point>())).Returns(true);
+      for (int i = 0; i < 11; i++)
+      {
+        _projectile.Fly();
+      }
+      _mockMap.Verify(m => m.RemoveMapObject(_projectile), Times.Once);
+    }
+
+    [Test]
+    public void Update_CallsFly()
+    {
+      var mockProjectile = new Mock<Projectile>(new Point(1, 1), Direction.Right, _mockScreenObjectManager.Object, 5, 10, Color.Red, _mockMap.Object) { CallBase = true };
+      mockProjectile.Object.SetMovementLogic(_mockMovementLogic.Object);
+      mockProjectile.Object.Update();
+      mockProjectile.Verify(p => p.Fly(), Times.Once);
+    }
   }
-
-  [Test]
-  public void Constructor_SetsProperties()
-  {
-    //var constructorTestGameobject = new Projectile(spawnPosition, direction, ScreenObjectManager, damage, maxDistance, color, _map, glyph);  
-    // var constructorTestProjectile = new Projectile(_position, _screenObjectManager.Object, _map.Object, _movementLogic.Object);
-    //Assert.That(constructorTestGameobject.Position, Is.EqualTo(_position));
-  }
-
-  [Test]
-  public void GetAppearance_ReturnsAppearance()
-  {
-    Assert.That(_gameObject.GetAppearance(), Is.EqualTo(_appearance));
-  }
-
-  [Test]
-  public void GetPosition_ReturnsPosition()
-  {
-    Assert.That(_gameObject.Position, Is.EqualTo(new Point(1, 1)));
-  }
-
-  [Test]
-  public void RemoveSelf_Calls_RemoveMapObject()
-  {
-    _gameObject.RemoveSelf();
-
-    _map.Verify(m => m.RemoveMapObject(_gameObject), Times.Once);
-  }
-
-  [Test]
-  public void Move_Calls_MoveOnMovementLogic()
-  {
-    var newPosition = new Point(2, 2);
-    _gameObject.Move(newPosition, _map.Object);
-
-    _movementLogic.Verify(m => m.Move(_gameObject, _map.Object, newPosition), Times.Once);
-  }
-
-  [Test]
-  public void Touched_Calls_TouchingOnSource()
-  {
-    var source = new Mock<IGameObject>();
-    _gameObject.Touched(source.Object);
-
-    source.Verify(m => m.Touching(_gameObject), Times.Once);
-  }
-
 }
-
